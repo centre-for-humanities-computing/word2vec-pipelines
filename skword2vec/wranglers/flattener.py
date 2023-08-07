929d248e587583ab31423c87ec4b8c35ee33695d
Márton Kardos <power.up1163@gmail.com>
2023-08-07 17:33:10 +0200

    Removed StreamFlattener

--- skword2vec/wranglers/flattener.py
+++ skword2vec/wranglers/flattener.py
@@ -1,52 +1,8 @@
-from typing import Iterable, Union
+from typing import Iterable
 
 import awkward as ak
 from numpy.typing import ArrayLike
 from sklearn.base import BaseEstimator, TransformerMixin
-
-from skword2vec.streams import flatten_stream
-
-
-class StreamFlattener(TransformerMixin, BaseEstimator):
-    """Flattens iterable stream along given axis.
-
-    The important distinction between this and ArrayFlattener,
-    is that StreamFlattener does computes lazily and creates a generator
-    instead of constructing an Awkward Array. This can have a positive
-    impact on performance especially when the data structure is
-    not a tensor to begin with.
-
-    Parameters
-    ----------
-    axis: int, default 1
-        Axis/level of depth at which the iterable should be flattened.
-    """
-
-    def __init__(self, axis: int = 1):
-        self.axis = axis
-
-    def transform(self, X: Iterable) -> Iterable:
-        """Flattens the given iterable along the axis.
-
-        Parameters
-        ----------
-        X: nested iterable
-            Iterable to flatten.
-
-        Returns
-        -------
-        iterable
-            Iterable that gets flattened along the given axis.
-        """
-        return flatten_stream(X, axis=self.axis)
-
-    def fit(self, X, y=None):
-        """Does nothing, exists for compatibility."""
-        return self
-
-    def partial_fit(self, X, y=None):
-        """Does nothing, exists for compatibility."""
-        return self
 
 
 class ArrayFlattener(TransformerMixin, BaseEstimator):
@@ -66,12 +22,12 @@ class ArrayFlattener(TransformerMixin, BaseEstimator):
     def __init__(self, axis: int = 1):
         self.axis = axis
 
-    def transform(self, X: Union[ArrayLike, ak.Array]) -> ak.Array:
+    def transform(self, X: ArrayLike) -> ak.Array:
         """Flattens the given array along the axis.
 
         Parameters
         ----------
-        X: ArrayLike or ak.Array
+        X: ArrayLike
             Array to flatten.
 
         Returns
