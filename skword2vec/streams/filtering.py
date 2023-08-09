281c089c7159e8782cf4784ec0b54e33134104d9
Márton Kardos <power.up1163@gmail.com>
2023-08-09 15:02:22 +0200

    Added docs to Stream, added prefit to model based filter

--- skword2vec/streams/filtering.py
+++ skword2vec/streams/filtering.py
@@ -4,10 +4,13 @@ from sklearn.base import BaseEstimator
 
 
 def filter_batches(
-    chunks: Iterable[list], estimator: BaseEstimator
+    chunks: Iterable[list], estimator: BaseEstimator, prefit: bool
 ) -> Iterable[list]:
     for chunk in chunks:
-        predictions = estimator.predict(chunk)  # type: ignore
+        if prefit:
+            predictions = estimator.predict(chunk)  # type: ignore
+        else:
+            predictions = estimator.fit_predict(chunk)  # type: ignore
         passes = predictions != -1
         filtered_chunk = [elem for elem, _pass in zip(chunk, passes) if _pass]
         yield filtered_chunk
